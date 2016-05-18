f8f367c5403ce9f27a937126c1824ffc58c6a359
Kazuya Ueoka <k.ueoka@timers-inc.com>
2016-05-18 11:16:47 +0900

    add carthage

---
+++ FKContactsBuild/FKContactsBuild.h
@@ -0,0 +1,19 @@
+//
+//  FKContactsBuild.h
+//  FKContactsBuild
+//
+//  Created by Kazuya Ueoka on 2016/05/18.
+//  Copyright © 2016年 Timers inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FKContactsBuild.
+FOUNDATION_EXPORT double FKContactsBuildVersionNumber;
+
+//! Project version string for FKContactsBuild.
+FOUNDATION_EXPORT const unsigned char FKContactsBuildVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FKContactsBuild/PublicHeader.h>
+
+
